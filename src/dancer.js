52800f97e77410eee4e0c16b03a35a80a80c0a11
Nick Havens <npahavens@gmail.com>
2017-06-09 14:19:30 -0500

    refactored .setPosition to be outside of Dancer constructor

--- src/dancer.js
+++ src/dancer.js
@@ -2,35 +2,30 @@
 var Dancer = function(top, left, timeBetweenSteps) {
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
-
+  this.top = top;
+  this.left = left;
+  this.timeBetweenSteps = timeBetweenSteps;
   // this.step = function() {
   //   // the basic dancer doesn't do anything interesting at all on each step,
   //   // it just schedules the next step
   //   setTimeout(this.step.bind(this), timeBetweenSteps);
   // };
-  this.step(timeBetweenSteps);
-
-  this.setPosition = function(top, left) {
-    //console.log('calling setPosition')
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-
-    this.$node.css(styleSettings);
-  };
-   //console.log(JSON.stringify(this));
-
-  this.setPosition(top, left);
-
-
+  this.step();
+  this.setPosition();
 };
 
- Dancer.prototype.step = function(timeBetweenSteps) {
-   console.log(timeBetweenSteps)
-   setTimeout(this.step.bind(this, timeBetweenSteps), timeBetweenSteps);
- }
+Dancer.prototype.step = function() {
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+};
+
+Dancer.prototype.setPosition = function() {
+  var styleSettings = {
+    top: this.top,
+    left: this.left
+  };
+  this.$node.css(styleSettings);
+};
+
+
 
 
