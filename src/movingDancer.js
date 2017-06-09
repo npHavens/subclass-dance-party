2e7526c355d8663b3b5532a1e58948ae2b394574
Nick Havens <npahavens@gmail.com>
2017-06-09 15:30:57 -0500

    fixed bug that allowed movingDancers to move out of screen

--- src/movingDancer.js
+++ src/movingDancer.js
@@ -15,16 +15,18 @@ MovingDancer.prototype.step = function() {
   this.newPosition.top = this.top + getRandomInt(-50, 50);
   this.newPosition.left = this.left + getRandomInt(-50, 50);
 
-  if (this.newPosition.top > this.height || this.top < 0) {
+  if (this.newPosition.top >= this.height || this.newPosition.top <= 0) {
     this.newPosition.top = this.top;
   }
-  if (this.newPosition.left > this.width || this.newPosition.left < 0) {
+  if (this.newPosition.left >= this.width || this.newPosition.left <= 0) {
     this.newPosition.left = this.left;
   }
 
   this.$node.animate(this.newPosition, this.timeBetweenSteps);
-  this.top = this.newPosition.top;
-  this.left = this.newPosition.left;
+  console.log(this.top, this.left)
+   this.top = this.newPosition.top;
+   this.left = this.newPosition.left;
+   console.log(this.top, this.left)
 };
 
 var getRandomInt = function(min, max) {
