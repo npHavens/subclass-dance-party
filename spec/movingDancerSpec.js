5f59f6396de374a4976d9b2bb6eb154c303d07b2
Nick Havens <npahavens@gmail.com>
2017-06-09 14:57:11 -0500

    refactored MovingDancer.Step to be outside of constructor

--- spec/movingDancerSpec.js
+++ spec/movingDancerSpec.js
@@ -1,33 +1,33 @@
-// describe('movingDancer', function() {
+describe('movingDancer', function() {
 
-//   var movingDancer, clock;
-//   var timeBetweenSteps = 100;
+  var movingDancer, clock;
+  var timeBetweenSteps = 100;
 
-//   beforeEach(function() {
-//     clock = sinon.useFakeTimers();
-//     movingDancer = new MovingDancer(10, 20, timeBetweenSteps);
-//   });
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    movingDancer = new MovingDancer(10, 20, timeBetweenSteps);
+  });
 
-//   it('should have a jQuery $node object', function() {
-//     expect(movingDancer.$node).to.be.an.instanceof(jQuery);
-//   });
+  it('should have a jQuery $node object', function() {
+    expect(movingDancer.$node).to.be.an.instanceof(jQuery);
+  });
 
-//   it('should have a step function that makes its node move', function() {
-//     var startPositionTop = movingDancer.position.top;
-//     var startPositionLeft = movingDancer.position.left;
-//     movingDancer.step();
-//     expect(movingDancer.position.top).to.not.equal(startPositionTop);
-//   });
+  it('should have a step function that makes its node move', function() {
+    var startPositionTop = movingDancer.top;
+    var startPositionLeft = movingDancer.left;
+    movingDancer.step();
+    expect(movingDancer.top).to.not.equal(startPositionTop);
+  });
 
-//   describe('dance', function() {
-//     it('should call step at least once per second', function() {
-//       sinon.spy(movingDancer, 'step');
-//       expect(movingDancer.step.callCount).to.be.equal(0);
-//       clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
-//       clock.tick(timeBetweenSteps);
-//       expect(movingDancer.step.callCount).to.be.equal(1);
-//       clock.tick(timeBetweenSteps);
-//       expect(movingDancer.step.callCount).to.be.equal(2);
-//     });
-//   });
-// });
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(movingDancer, 'step');
+      expect(movingDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+      expect(movingDancer.step.callCount).to.be.equal(1);
+      clock.tick(timeBetweenSteps);
+      expect(movingDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
--- src/movingDancer.js
+++ src/movingDancer.js
@@ -1,42 +1,36 @@
 var MovingDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass('moving-dancer');
-
-  this.position = {top: top, left: left};
-  this.oldStep = this.step;
-
-  this.step = function() {
-    this.oldStep();
-    this.height = $(document).height();
-    this.width = $(document).width();
-
-    this.newPosition = {};
-    this.newPosition.top = this.position.top + getRandomInt(-50, 50);
-    this.newPosition.left = this.position.left + getRandomInt(-50, 50);
-
-    if (this.newPosition.top > this.height || this.newPosition.top < 0) {
-      this.newPosition.top = this.position.top;
-    }
-    if (this.newPosition.left > this.width || this.newPosition.left < 0) {
-      this.newPosition.left = this.position.left;
-    }
-
-    this.$node.animate(this.newPosition, timeBetweenSteps);
-    this.position = this.newPosition;
-
-  };
-
-  this.getPosition = function() {
-    return this.$node.offset();
-  };
-
 };
 
 MovingDancer.prototype = Object.create(BlinkyDancer.prototype);
 MovingDancer.prototype.constructor = MovingDancer;
+
+MovingDancer.prototype.step = function() {
+  Dancer.prototype.step.call(this);
+  this.height = $(document).height();
+  this.width = $(document).width();
+
+  this.newPosition = {};
+  this.newPosition.top = this.top + getRandomInt(-50, 50);
+  this.newPosition.left = this.left + getRandomInt(-50, 50);
+
+  if (this.newPosition.top > this.height || this.top < 0) {
+    this.newPosition.top = this.top;
+  }
+  if (this.newPosition.left > this.width || this.newPosition.left < 0) {
+    this.newPosition.left = this.left;
+  }
+
+  this.$node.animate(this.newPosition, this.timeBetweenSteps);
+  this.top = this.newPosition.top;
+  this.left = this.newPosition.left;
+};
 
 var getRandomInt = function(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 };
+
+
