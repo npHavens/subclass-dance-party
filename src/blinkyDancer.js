87e21aa9d3fd21a6caa3416545826b8bb73d1d8a
Nick Havens <npahavens@gmail.com>
2017-06-09 14:03:24 -0500

    refactored Dancer to use methods outside of constructor

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -10,7 +10,7 @@ var BlinkyDancer = function(top, left, timeBetweenSteps) {
 
   this.step = function() {
     // call the old version of step at the beginning of any call to this new version of step
-    this.oldStep();
+    this.oldStep(timeBetweenSteps);
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
@@ -21,6 +21,7 @@ var BlinkyDancer = function(top, left, timeBetweenSteps) {
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
 BlinkyDancer.prototype.constructor = BlinkyDancer;
+
 
 
 
--- src/dancer.js
+++ src/dancer.js
@@ -1,15 +1,14 @@
 // Creates and returns a new dancer object that can step
 var Dancer = function(top, left, timeBetweenSteps) {
-
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
 
-  this.step = function() {
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(this.step.bind(this), timeBetweenSteps);
-  };
-  this.step();
+  // this.step = function() {
+  //   // the basic dancer doesn't do anything interesting at all on each step,
+  //   // it just schedules the next step
+  //   setTimeout(this.step.bind(this), timeBetweenSteps);
+  // };
+  this.step(timeBetweenSteps);
 
   this.setPosition = function(top, left) {
     //console.log('calling setPosition')
@@ -26,19 +25,12 @@ var Dancer = function(top, left, timeBetweenSteps) {
 
   this.setPosition(top, left);
 
-  // use jQuery to create an HTML <span> tag
-
-
-
-  //this.step(timeBetweenSteps);
-
-
-  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
-  // this one sets the position to some random default point within the body
-
-  //this.setPosition(top, left);
-  //console.log(JSON.stringify(this));
 
 };
+
+ Dancer.prototype.step = function(timeBetweenSteps) {
+   console.log(timeBetweenSteps)
+   setTimeout(this.step.bind(this, timeBetweenSteps), timeBetweenSteps);
+ }
 
 
